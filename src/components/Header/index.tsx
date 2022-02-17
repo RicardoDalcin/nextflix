fea7b8c220c0817bc311f3765f5915d1277c5e1b
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2022-02-16 22:05:04 -0300

    ✨ Create the home page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Logo } from './styles';
+
+const Header: React.FC = () => (
+  <Container>
+    <Logo src="/logo.svg" alt="Netflix" />
+  </Container>
+);
+
+export default Header;
